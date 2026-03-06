5cf6d1b24e50b55592b2cce23bea13f889e0d1e3
LambdaPyro <60356927+LambdaPyro@users.noreply.github.com>
2026-03-06 15:31:38 -0500

    Documentation and adjustments for UX

--- scripts/input_output.gd
+++ scripts/input_output.gd
@@ -1,33 +1,37 @@
 @icon("res://addons/Level-Design-Logic/images/InputOutput.svg")
 @tool
 extends Node3D
+## Series of Outputs that are run once an input is recieved.
+
 class_name InputOutput
 
-var undo_redo : EditorUndoRedoManager
-
+## Triggers the [InputOutput] to run [Output]s.
 signal input_triggered
+## Sent once all [Output]s have been triggered.
 signal outputs_triggered
 
-#RELOAD PROJECT WHEN ALTERING
-@export_tool_button("Add Output", "Add") var button = create_output
+## Creates a new [Output] node as a child
+@export_tool_button("Add Output", "Add") var _button = _create_output
 
-var last_output
+var _last_output
 
 func _ready() -> void:
-	input_triggered.connect(run_outputs)
+	input_triggered.connect(_run_outputs)
 
-var output : PackedScene = preload("res://addons/Level-Design-Logic/scenes/Output.tscn")
-func create_output() -> void:
-	var new_output : Output = output.instantiate()
+var _output : PackedScene = preload("res://addons/Level-Design-Logic/scenes/Output.tscn")
+func _create_output() -> void:
+	var new_output : Output = _output.instantiate()
 	var output_name = "Output_" + str(get_child_count())
 	new_output.name = output_name
 	add_child(new_output)
 	new_output.owner = get_tree().edited_scene_root
 
+## Remove the last added [Output] from the hierarchy.
+## @experimental
 func remove_last_output():
-	get_node(last_output).queue_free()
+	get_node(_last_output).queue_free()
 
-func run_outputs():
+func _run_outputs():
 	for o in get_children():
 		o.run_output()
 		if o.delay != 0 and o.delay_next_output:
--- scripts/output.gd
+++ scripts/output.gd
@@ -1,17 +1,30 @@
 @icon("res://addons/Level-Design-Logic/images/ArrowUp.svg")
 extends Node
-
+## Child of InputOutput, sends a signal after running all logic. Includes options for delaying both current and following [Output]s.
 class_name Output
 
+## Sent once all logic is ran. Connect to anything you want triggered.
 signal output_triggered
+## Triggers the parent [InputOutput] to continue the logic if there is a delay.
 signal continue_logic
 
-@export_range(0.0,10.0,.1, "or_greater") var delay : float = 0
+## How long the [Output] should wait before sending signals.
+@export_custom(PROPERTY_HINT_NONE, "suffix: Seconds") var delay : float = 0:
+	set(val):
+		delay = val
+		notify_property_list_changed()
+## Determines if the succeeding [Output] should await for this one to finish.
 @export var delay_next_output : bool = false
+
+func _validate_property(property: Dictionary):
+	if property.name == "delay_next_output" and delay == 0:
+		delay_next_output = false
+		property.usage |= PROPERTY_USAGE_READ_ONLY
 
 func _ready() -> void:
 	assert(get_parent() is InputOutput, str(self) + " needs to be child of type 'InputOutput'")
 
+## Runs logic of the [Output] with given parameters.
 func run_output() -> void:
 	var input_output = get_parent()
 	
