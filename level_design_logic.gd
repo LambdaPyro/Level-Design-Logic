f427f6c63ce3e536ab39e1e818c9cf37f811a130
LambdaPyro <60356927+LambdaPyro@users.noreply.github.com>
2026-03-08 20:26:47 -0400

    1.0 Boolean Switch

--- level_design_logic.gd
+++ level_design_logic.gd
@@ -1,9 +1,11 @@
 @tool
 extends EditorPlugin
 
-const IOGIZMO = preload("res://addons/Level-Design-Logic/gizmos/io_gizmo.gd")
+const IoGizmo = preload("res://addons/Level-Design-Logic/gizmos/io_gizmo.gd")
+const BsGizmo = preload("res://addons/Level-Design-Logic/gizmos/bs_gizmo.gd")
 
-var io_gizmo = IOGIZMO.new()
+var io_gizmo = IoGizmo.new()
+var bs_gizmo = BsGizmo.new()
 
 func _enable_plugin() -> void:
 	# Add autoloads here.
@@ -18,10 +20,12 @@ func _disable_plugin() -> void:
 func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	add_node_3d_gizmo_plugin(io_gizmo)
+	add_node_3d_gizmo_plugin(bs_gizmo)
 	pass
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	remove_node_3d_gizmo_plugin(io_gizmo)
+	remove_node_3d_gizmo_plugin(bs_gizmo)
 	pass
