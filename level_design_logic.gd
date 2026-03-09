f9de7716444d5db045bd0622f61cbdd9b23fb3c2
LambdaPyro <60356927+LambdaPyro@users.noreply.github.com>
2026-03-09 17:40:40 -0400

    1.0 Trigger volume

--- level_design_logic.gd
+++ level_design_logic.gd
@@ -3,9 +3,11 @@ extends EditorPlugin
 
 const IoGizmo = preload("res://addons/Level-Design-Logic/gizmos/io_gizmo.gd")
 const BsGizmo = preload("res://addons/Level-Design-Logic/gizmos/bs_gizmo.gd")
+const TrGizmo = preload("res://addons/Level-Design-Logic/gizmos/tr_gizmo.gd")
 
 var io_gizmo = IoGizmo.new()
 var bs_gizmo = BsGizmo.new()
+var tr_gizmo = TrGizmo.new()
 
 func _enable_plugin() -> void:
 	# Add autoloads here.
@@ -21,11 +23,11 @@ func _enter_tree() -> void:
 	# Initialization of the plugin goes here.
 	add_node_3d_gizmo_plugin(io_gizmo)
 	add_node_3d_gizmo_plugin(bs_gizmo)
-	pass
+	add_node_3d_gizmo_plugin(tr_gizmo)
 
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
 	remove_node_3d_gizmo_plugin(io_gizmo)
 	remove_node_3d_gizmo_plugin(bs_gizmo)
-	pass
+	remove_node_3d_gizmo_plugin(tr_gizmo)
